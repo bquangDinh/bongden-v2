31827b30f202b8e40ae26b70382455b05bfc8801
bquangDinh <buiquangdinh1751@gmail.com>
2019-07-03 14:46:34 +0700

    Fix updating page

--- public/js/userpage/update_article.js
+++ public/js/userpage/update_article.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
     height: 500,
     theme: 'silver',
     plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
-  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat | tiny_mce_wiris_formulaEditor | tiny_mce_wiris_formulaEditorChemistry',
   image_advtab: true,
   templates: [
     { title: 'Test template 1', content: 'Test 1' },
@@ -56,7 +56,11 @@ $(document).ready(function(){
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css'
-  ]
+  ],
+  external_plugins: {
+        tiny_mce_wiris: 'https://www.wiris.net/demo/plugins/tiny_mce/plugin.js'
+    },
+  paste_data_images: true,
   });
 
   async function getCoverChoosen(){
